2b8a407563e50832745c8b6c5e447e6eaf40b775
Markus Monz <markusmonz@googlemail.com>
2018-04-17 16:20:07 +0200

    add: implemented simple disocvery service
    created a service interface

--- net/receive.go
+++ net/receive.go
@@ -3,24 +3,68 @@ package net
 import (
         "net"
         "log"
-        "sync"
+        //"sync"
         "time"
         "github.com/google/uuid"
 )
 
-func Receive(s *net.UDPConn, msgSize int, wg sync.WaitGroup) {
-        defer wg.Done()
+const MSG_SIZE = 36
+
+type service interface {
+        Start()
+        Stop()
+}
+
+type discoveryService struct {
+        uuid uuid.UUID
+        c *net.UDPConn
+        initDelay time.Duration
+        period time.Duration
+}
+
+func NewDiscoveryService(initDelay time.Duration, period time.Duration) *discoveryService {
+        s := new(discoveryService)
+        s.initDelay = initDelay
+        s.period = period
+
+        localAddress, err := net.ResolveUDPAddr("udp", "0.0.0.0:9942")
+        if err != nil {
+                log.Fatal(err)
+        }
+        s.c, err = net.ListenUDP("udp", localAddress)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        // generate unique node id (UUID)
+        s.uuid = uuid.New()
+
+        return s
+}
+
+func (s discoveryService) Start() {
+        go s.receive()
+        go s.send()
+}
+
+func (s discoveryService) Stop() {
+        // currently will crash due to log.Fatal on closed connection!
+        s.c.Close()
+}
+
+func (s discoveryService) receive() {
+        log.Println("in receive", s.initDelay, s.period)
         for {
-                buf := make([]byte, msgSize)
-                s.ReadFrom(buf)
+                buf := make([]byte, MSG_SIZE)
+                s.c.ReadFrom(buf)
                 log.Println(string(buf))
         }
 }
 
-func Send(s *net.UDPConn, msgSize int, initialDelay time.Duration, period time.Duration, wg sync.WaitGroup) {
-        defer wg.Done()
+func (s discoveryService) send() {
+        log.Println("in send", s.initDelay, s.period)
         // wait initial delay
-        time.Sleep(time.Second * initialDelay)
+        time.Sleep(time.Second * s.initDelay)
 
         // define broadcast address
         destAddress, err := net.ResolveUDPAddr("udp", "255.255.255.255:9942")
@@ -28,25 +72,23 @@ func Send(s *net.UDPConn, msgSize int, initialDelay time.Duration, period time.D
                 log.Fatal(err)
         }
 
-        // generate unique node id (UUID)
-        uuid := uuid.New()
-        buf, err := uuid.MarshalText()
+        // convert uuid to []byte
+        buf, err := s.uuid.MarshalText()
         if err != nil {
                 log.Fatal(err)
         }
-
+        // send uuid
         for {
-                // send uuid
                 log.Println("send id")
 
-                n, err := s.WriteToUDP(buf, destAddress)
+                n, err := s.c.WriteToUDP(buf, destAddress)
                 if err != nil {
                         log.Fatal(err)
                 }
-                if n != msgSize {
+                if n != MSG_SIZE {
                         log.Fatal("Could not write discovery message properly!")
                 }
                 // wait
-                time.Sleep(time.Second * period)
+                time.Sleep(time.Second * s.period)
         }
 }
